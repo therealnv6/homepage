71170ca985e9416b4f8d98cf5221e7222046e86f
therealnv6 <devrawr@protonmail.com>
2023-07-03 14:05:29 +0200

    update (idk what i did)

--- pages/api/wallhaven.tsx
+++ pages/api/wallhaven.tsx
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-  req: NextApiRequest,
+  _: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {

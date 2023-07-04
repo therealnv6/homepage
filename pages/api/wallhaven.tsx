986a6e6d330f5aaa861f38c5c9be128a857d87d3
therealnv6 <devrawr@protonmail.com>
2023-07-04 06:17:43 +0200

    change wallhaven-centric image api to use unsplash, with wallhaven as a backup

--- pages/api/wallhaven.tsx
+++ pages/api/wallhaven.tsx
@@ -1,13 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const unsplashResponse = await fetch(
+      "https://api.unsplash.com/photos/random?query=forest",
+      {
+        headers: {
+          "Authorization": `Client-ID ${process.env.UNPLASH_TOKEN}`,
+        },
+      },
+    );
+
+    if (!unsplashResponse.ok) {
+      await alt_wallhaven(req, res);
+      return;
+    }
+
+    const { urls } = await unsplashResponse.json();
+    res.status(200).json({ path: urls.regular });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Failed to get random picture from Unsplash!",
+    });
+  }
+}
+
+export async function alt_wallhaven(
   _: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
     const query = "forest";
     const response = await fetch(
-      `https://wallhaven.cc/api/v1/search?q=${query}&purity=111&sorting=random`,
+      `https://wallhaven.cc/api/v1/search?q=${query}&purity=100&sorting=random`,
       {
         credentials: "include",
         headers: {
